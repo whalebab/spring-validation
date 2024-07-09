9b5a12388bb1453e93378f437fb22a66e950ae5b
whalebab <juncoder94@gmail.com>
2024-07-09 23:51:51 +0900

    Bean Validation 개념정리 - 1

---
+++ src/main/java/hello/itemservice/domain/item/SaveCheck.java
@@ -0,0 +1,5 @@
+package hello.itemservice.domain.item;
+
+// 저장용 그룹 표시할 때
+public interface SaveCheck {
+}
---
+++ src/main/java/hello/itemservice/domain/item/UpdateCheck.java
@@ -0,0 +1,5 @@
+package hello.itemservice.domain.item;
+
+// 수정용 그룹 표시할 때
+public interface UpdateCheck {
+}
